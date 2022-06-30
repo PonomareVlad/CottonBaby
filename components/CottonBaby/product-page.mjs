7ea957cbf276cce12244005168cbd8be54c05b2b
PonomareVlad <Git@PonomareVlad.ru>
2022-06-30 20:33:17 +0500

    Product page title

--- components/CottonBaby/product-page.mjs
+++ components/CottonBaby/product-page.mjs
@@ -6,7 +6,53 @@ export class ProductPage extends LitElement {
     static get styles() {
         return [styles, css`
           :host {
+            display: block;
             padding-top: var(--header-height);
+          }
+
+          .title-row {
+            padding-top: 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-start;
+          }
+
+          .title, .price {
+            font-size: 24px;
+          }
+
+          .title {
+            margin-top: 0;
+            line-height: 34px;
+            margin-bottom: 5px;
+          }
+
+          .product-code {
+            margin-top: 0;
+          }
+
+          .price {
+            padding: 0 1ch;
+            line-height: 32px;
+            border-radius: 30px;
+            white-space: nowrap;
+            display: inline-block;
+            border: solid 1px black;
+          }
+
+          .price:after {
+            content: ' ₽';
+            font-family: HelveticaNeue, sans-serif, -apple-system;
+          }
+
+          .product-code {
+            color: #b1b1b1;
+            font-size: 16px;
+            font-weight: bold;
+          }
+
+          .product-code:before {
+            content: 'Артикул: '
           }
 
           @media (min-width: 1024px) {
@@ -21,7 +67,14 @@ export class ProductPage extends LitElement {
         this?.setMeta({title: 'Product'})
         return html`
             <product-gallery></product-gallery>
-            <div class="root-padding"><h1>Название продукта</h1></div>
+            <div class="root-padding title-row">
+                <div class="title-content">
+                    <h1 class="title">Название продукта</h1>
+                    <p class="product-code">123456789</p>
+                </div>
+                <div class="price">100</div>
+            </div>
+            <div class="root-padding">Состав: 100% хлопок</div>
         `
     }
 }
